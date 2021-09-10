16e13c5e1a452c9e5484ced9b522a1314631b132
David Wei <david.wei@koalatech.co.nz>
2021-09-10 13:12:35 +1200

    fix the tests

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -16,7 +16,6 @@ namespace GildedRoseTests
         {
             var fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
 
             Program.Main(new string[] { });
             var output = fakeoutput.ToString();
