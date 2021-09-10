f2809ed3d1ca02339191473d217a564450fe46dd
David Wei <david.wei@koalatech.co.nz>
2021-09-10 13:31:29 +1200

    the second step ok

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -45,68 +45,19 @@ namespace GildedRoseKata
             {
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality < 50)
                     {
-                        if (item.Quality > 0)
+                        item.Quality = item.Quality + 1;
+
+                        if (item.SellIn < 11)
                         {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            if (item.Quality < 50)
                             {
-                                item.Quality = item.Quality - 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
 
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.SellIn = item.SellIn - 1;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Name != "Aged Brie")
-                        {
-                            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.Quality > 0)
-                                {
-                                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                    {
-                                        item.Quality = item.Quality - 1;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                item.Quality = item.Quality - item.Quality;
-                            }
-                        }
-                        else
+                        if (item.SellIn < 6)
                         {
                             if (item.Quality < 50)
                             {
@@ -114,42 +65,26 @@ namespace GildedRoseKata
                             }
                         }
                     }
+
+
+                    item.SellIn = item.SellIn - 1;
+
+                    if (item.SellIn < 0)
+                    {
+                        {
+                            item.Quality = item.Quality - item.Quality;
+                        }
+                    }
                 }
                 else
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (true)
                     {
                         if (item.Quality > 0)
                         {
                             if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
                                 item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
                             }
                         }
                     }
